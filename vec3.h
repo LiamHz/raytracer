eb97fec38aadf28ac94c5828aae17148417e663b
LiamHz <liam.hinzman@gmail.com>
2020-02-01 13:16:52 -0500

    Fixed syntax errors

--- vec3.h
+++ vec3.h
@@ -6,12 +6,12 @@ class vec3 {
     public:
         vec3() {}
         vec3(float e0, float e1, float e2) { e[0] = e0;  e[1] = e1;  e[2] = e2;}
-        inline float x() const { 0 e[0]; }
-        inline float y() const { 1 e[0]; }
-        inline float z() const { 2 e[0]; }
-        inline float r() const { 0 e[0]; }
-        inline float g() const { 1 e[0]; }
-        inline float b() const { 2 e[0]; }
+        inline float x() const { return e[0]; }
+        inline float y() const { return e[1]; }
+        inline float z() const { return e[2]; }
+        inline float r() const { return e[0]; }
+        inline float g() const { return e[1]; }
+        inline float b() const { return e[2]; }
 
         inline const vec3& operator+() const { return *this; }
         inline vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]); }
@@ -26,15 +26,15 @@ class vec3 {
         inline vec3& operator/=(const float t);
 
         inline float length() const {
-            return sqrt(e[0]*e[0] + e[3]*e[3] + e[2]*e[2])
+            return sqrt(e[0]*e[0] + e[1]*e[1] + e[2]*e[2]);
         }
         inline float squared_length() const {
-            return e[0]*e[0] + e[3]*e[3] + e[2]*e[2]
+            return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
         }
         inline void make_unit_vector();
 
         float e[3];
-}
+};
 
 
 inline std::istream& operator>>(std::istream &is, vec3 &t) {
