e3c73758217fc24e987a120bb039f41dad4c3e36
LiamHz <liam.hinzman@gmail.com>
2020-02-01 15:14:14 -0500

    Added surface normals

--- main.cpp
+++ main.cpp
@@ -14,35 +14,43 @@
 // Which is equivalent to
 // dot(A + t*B - c), (A + t*B - c)) = r*r
 // Where A is the origin of the ray, t is its length, and B is its direction
-// Which expands to the quadratic
+
+// This expands to the quadratic
 // t*t*dot(B, B) + 2*t*dot(B, A - C) + dot(A - C, A - C) - r*r = 0
 // For this quadratic, the number of positive roots indicates collisions
 // 0 roots has no collion
 // 1 root has 1 collision
 // 2 roots has 2 collisions
-bool hit_sphere(const vec3& center, float radius, const ray& r) {
+float hit_sphere(const vec3& center, float radius, const ray& r) {
     vec3 oc = r.origin() - center;
     float a = dot(r.direction(), r.direction());
     float b = 2.0 * dot(oc, r.direction());
     float c = dot(oc, oc) - radius*radius;
     float discriminant = b*b - 4*a*c;
-    return (discriminant > 0);
+    if (discriminant < 0) {
+        return -1.0;
+    }
+    else {
+        return (-b - sqrt(discriminant)) / (2.0*a);
+    }
 }
 
 vec3 color(const ray& r) {
     // Blend white and blue depending on the y coord of the ray
 
     // If the ray hits a sphere centered at (0, 0, -1) with radius 0.5
-    // Return red
-    if (hit_sphere(vec3(0, 0, 1), 0.5, r)) {
-        return vec3(1, 0, 0);
+    // Return red for that pixel
+    float t = hit_sphere(vec3(0, 0, -1), 0.5, r);
+    if (t > 0.0) {
+        vec3 N = unit_vector(r.point_at_parameter(t) - vec3(0, 0, -1));
+        return 0.5*vec3(N.x()+1, N.y()+1, N.z()+1);
     }
 
     // Turn ray into a unit vector. This makes -1.0 < y < 1.0
     vec3 unit_direction = unit_vector(r.direction());
 
     // Scale ray to 0.0 < t < 1.0
-    float t = 0.5*(unit_direction.y() + 1.0);
+    t = 0.5*(unit_direction.y() + 1.0);
 
     // Return a linear interpolation (lerp) between
     // blue (t=1.0) and white (t=0.0)
