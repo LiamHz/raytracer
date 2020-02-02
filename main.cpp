159ff5bc51dc17ac87c629a6865aa0f6977e5447
LiamHz <liam.hinzman@gmail.com>
2020-02-01 22:58:47 -0500

    Automated scene creation

--- main.cpp
+++ main.cpp
@@ -39,11 +39,59 @@ vec3 color(const ray& r, hitable *world, int depth) {
     }
 }
 
+hitable *random_scene() {
+    int n = 500;
+    hitable **list = new hitable*[n+1];
+
+    // The sphere which all others sit upon
+    list[0] = new sphere(vec3(0, -1000, 0), 1000, new lambertian(vec3(0.5, 0.5, 0.5)));
+
+    int i = 1;
+    for (int a = -11; a < 11; a++) {
+        for (int b = -11; b < 11; b++) {
+            float choose_mat = drand48();
+            vec3 center(a+0.9*drand48(), 0.2, b+0.9*drand48());
+            if ((center-vec3(4,0.2,0)).length() > 0.9) {
+                //  Diffuse
+                if (choose_mat < 0.8) {
+                    list[i++] = new sphere(
+                        center, 0.2,
+                        new lambertian(vec3(drand48()*drand48(),
+                                            drand48()*drand48(),
+                                            drand48()*drand48()))
+                    );
+                }
+                // Metal
+                else if (choose_mat < 0.95) {
+                    list[i++] = new sphere(
+                        center, 0.2,
+                        new metal(vec3(0.5*(1 + drand48()),
+                                       0.5*(1 + drand48()),
+                                       0.5*(1 + drand48())),
+                                    0.5*drand48())
+                    );
+                }
+                // Glass
+                else {
+                    list[i++] = new sphere(center, 0.2, new dielectric(1.5));
+                }
+            }
+        }
+    }
+
+    list[i++] = new sphere(vec3(0, 1, 0), 1.0, new dielectric(1.5));
+    list[i++] = new sphere(vec3(-4, 1, 0), 1.0, new lambertian(vec3(0.4, 0.2, 0.1)));
+    list[i++] = new sphere(vec3(4, 1, 0), 1.0, new metal(vec3(0.7, 0.6, 0.5), 0.0));
+
+    return new hitable_list(list, i);
+}
+
+
 int main() {
     // Set the width and height of canvas
-    int nx = 480;
-    int ny = 360;
-    int ns = 10;
+    int nx = 1920;
+    int ny = 1080;
+    int ns = 50;
 
     // Create a ppm file to store the image data
     std::ofstream ofs;
@@ -66,20 +114,14 @@ int main() {
 
     ofs << "P3\n" << nx << " " << ny << "\n255\n";
 
-    // Create a list of hitable objects
-    hitable *list[5];
-    list[0] = new sphere(vec3(0, 0, -1), 0.5, new lambertian(vec3(0.8, 0.3, 0.3)));
-    list[1] = new sphere(vec3(0, -100.5, -1), 100, new lambertian(vec3(0.8, 0.8, 0.0)));
-    list[2] = new sphere(vec3(1, 0, -1), 0.5, new metal(vec3(0.8, 0.6, 0.2), 0.3));
-    list[3] = new sphere(vec3(-1, 0, -1), 0.5, new dielectric(1.5));
-    list[4] = new sphere(vec3(-1, 0, -1), -0.45, new dielectric(1.5));
-    hitable *world = new hitable_list(list,5);
+    // Create hitable objects
+    hitable *world = random_scene();
 
     // Instantiate camera
-    vec3 lookfrom(3, 3, 2);
-    vec3 lookat(0, 0, -1);
-    float dist_to_focus = (lookfrom - lookat).length();
-    float aperture = 2.0;
+    vec3 lookfrom(13, 2, 3);
+    vec3 lookat(0, 0, 0);
+    float dist_to_focus = 10.0; //(lookfrom - lookat).length();
+    float aperture = 0.1;
 
     camera cam(lookfrom, lookat, vec3(0, 1, 0), 20, float(nx) / float(ny), aperture, dist_to_focus);
 
