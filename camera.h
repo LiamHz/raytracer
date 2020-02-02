ada48b5a96d565f5663b5374f9813d1678a87df5
LiamHz <liam.hinzman@gmail.com>
2020-02-01 21:51:15 -0500

    Implemented positionable camera

--- camera.h
+++ camera.h
@@ -4,22 +4,36 @@
 #include "ray.h"
 
 class camera {
-    public:
-        camera() {
-            // Define dimensions of the camera
-            // Horizontal and vertical define the other corners of the canvas
-            // relative to the lower_left_corner
-            lower_left_corner = vec3(-2.0, -1.0, -1.0);
-            horizontal = vec3(4.0, 0.0, 0.0);
-            vertical = vec3(0.0, 2.0, 0.0);
-            origin = vec3(0.0, 0.0, 0.0);
-        }
-        ray get_ray(float u, float v) { return ray(origin, lower_left_corner + u*horizontal + v*vertical - origin); }
+public:
+    // vfov is top to bottom of camera view in degrees
+    camera(vec3 lookfrom, vec3 lookat, vec3 vup, float vfov, float aspect) {
+        vec3 u, v, w;
 
-        vec3 origin;
-        vec3 lower_left_corner;
-        vec3 horizontal;
-        vec3 vertical;
+        // The height and width of the camera view are determined by the vfov
+        float theta = vfov*M_PI/180;
+        float half_height = tan(theta/2);
+        float half_width = aspect * half_height;
+
+        // Define the location and orientation of the camera
+        origin = lookfrom;
+        w = unit_vector(lookfrom - lookat);
+        u = unit_vector(cross(vup, w));
+        v = cross(w, u);
+
+        // Define dimensions of the camera
+        // Horizontal and vertical define the other corners of the canvas
+        // relative to the lower_left_corner
+        // lower_left_corner = vec3(-half_width, -half_height, -1.0);
+        lower_left_corner = origin -half_width*u -half_height*v - w;
+        horizontal = 2 * half_width * u;
+        vertical = 2 * half_height * v;
+    }
+    ray get_ray(float u, float v) { return ray(origin, lower_left_corner + u*horizontal + v*vertical - origin); }
+
+    vec3 origin;
+    vec3 lower_left_corner;
+    vec3 horizontal;
+    vec3 vertical;
 };
 
 #endif
--- main.cpp
+++ main.cpp
@@ -68,15 +68,15 @@ int main() {
 
     // Create a list of hitable objects
     hitable *list[5];
-    list[0] = new sphere(vec3(0,0,-1), 0.5, new lambertian(vec3(0.8, 0.3, 0.3)));
-    list[1] = new sphere(vec3(0,-100.5,-1), 100, new lambertian(vec3(0.8, 0.8, 0.0)));
+    list[0] = new sphere(vec3(0, 0, -1), 0.5, new lambertian(vec3(0.8, 0.3, 0.3)));
+    list[1] = new sphere(vec3(0, -100.5, -1), 100, new lambertian(vec3(0.8, 0.8, 0.0)));
     list[2] = new sphere(vec3(1, 0, -1), 0.5, new metal(vec3(0.8, 0.6, 0.2), 0.3));
     list[3] = new sphere(vec3(-1, 0, -1), 0.5, new dielectric(1.5));
     list[4] = new sphere(vec3(-1, 0, -1), -0.45, new dielectric(1.5));
     hitable *world = new hitable_list(list,5);
 
     // Instantiate camera
-    camera cam;
+    camera cam(vec3(-2, 2, 1), vec3(0, 0, -1), vec3(0, 1, 0), 90, float(nx) / float(ny));
 
     // Write pixels out in rows from left to right (int i)
     // Write rows from top to bottom (int j)
